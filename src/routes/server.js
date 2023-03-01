e1e07ab61ac4f47360977f6c2e41b11fa2cdc266
Martin Resnicoff <mresnicoff@gmail.com>
2023-03-01 10:29:48 -0300

    Second commit

--- src/routes/server.js
+++ src/routes/server.js
@@ -2,7 +2,7 @@ const { router } = require("../routes/index");
 const express = require("express");
 const cors = require("cors");
 const server = express();
-const PORT = 3001;
+const PORT = require("../config.js");
 server.use(cors());
 server.use(express.json());
 server.use("/rickandmorty/", router);
